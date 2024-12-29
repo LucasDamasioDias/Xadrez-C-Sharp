3120549682793f62a15824ab256cfdb4bd254d4d
Lucas Damásio <lucas_damasio@hotmail.com>
2024-12-28 21:32:15 -0300

    Tratando alguns erros com exceções personalizadas

--- xadrez/Rei.cs
+++ xadrez/Rei.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using tabuleiro;
 
-namespace Xadrez.xadrez
+namespace xadrez
 {
-    internal class Rei
+    class Rei : Peca
     {
+        public Rei (Tabuleiro tab, Cor cor) : base (tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
     }
 }
--- xadrez/Torre.cs
+++ xadrez/Torre.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using tabuleiro;
 
-namespace Xadrez.xadrez
+namespace xadrez
 {
-    internal class Torre
+    class Torre : Peca
     {
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
     }
 }

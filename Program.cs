c71d07a8f858593d447c74faa418af523d888bd6
Lucas Damásio <lucas_damasio@hotmail.com>
2024-12-29 21:23:12 -0300

    Mudando as cores de peça e melhorando a classe Tela

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿using tabuleiro;
+using xadrez;
 using Xadrez;
 
 Tabuleiro tab = new Tabuleiro(8,8);
 
+
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
+
 Tela.imprimirTabuleiro(tab);
+
 Console.ReadLine();

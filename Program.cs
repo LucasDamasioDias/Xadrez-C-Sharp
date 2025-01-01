5e85812208db239c67e0e5529ca5e1b3d8c9c27e
Lucas Damásio <lucas_damasio@hotmail.com>
2025-01-01 20:41:32 -0300

    Implemento de restrições de movimento, inclusão dos try/catch

--- Program.cs
+++ Program.cs
@@ -2,27 +2,48 @@
 using xadrez;
 using Xadrez;
 
-PartidaDeXadrez partida = new PartidaDeXadrez();
-
-while (!partida.terminada)
+try
 {
-    Console.Clear();
+    PartidaDeXadrez partida = new PartidaDeXadrez();
+
+    while (!partida.terminada)
+    {
+        try
+        {
+            Console.Clear();
+            Tela.imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            Console.WriteLine("Turno: " + partida.turno);
+            Console.WriteLine("Aguardando jogada de: " + partida.jogadorAtual);
+            Console.WriteLine();
+
+            Console.Write("Origem: ");
+            Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+            partida.validarPosicaoOrigem(origem);
+
+            bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+            Console.Clear();
+            Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+            Console.Write("Destino: ");
+            Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+            partida.validarPosicaoDestino(origem, destino);
+
+            partida.realizaJogada(origem, destino);
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.ReadLine();
+        }
+    }
+
     Tela.imprimirTabuleiro(partida.tab);
 
-    Console.Write("Origem: ");
-    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-
-    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
-
-    Console.Clear();
-    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-
-    Console.Write("Destino: ");
-    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
-    partida.executarMovimento(origem, destino);
+    Console.ReadLine();
 }
-
-
-Tela.imprimirTabuleiro(partida.tab);
-
-Console.ReadLine();
+catch (TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}

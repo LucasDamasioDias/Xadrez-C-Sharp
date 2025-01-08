447bac766a652b77b67ba79aa1aba1c7e2ff7e77
Lucas Damásio <lucas_damasio@hotmail.com>
2025-01-08 20:32:17 -0300

    Correção do Roque

--- xadrez/Rei.cs
+++ xadrez/Rei.cs
@@ -77,28 +77,28 @@ namespace xadrez
 
             if (qteMovimentos == 0 && !partida.xeque)
             {              
-                Posicao T1 = new Posicao(pos.linha, pos.coluna + 3);
+                Posicao T1 = new Posicao(posicao.linha, posicao.coluna + 3);
                 if (tab.posicaoValida(T1) && testeRoque(T1))
                 {
-                    Posicao p1 = new Posicao(pos.linha, pos.coluna + 1);
-                    Posicao p2 = new Posicao(pos.linha, pos.coluna + 2);
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
                     if (tab.posicaoValida(p1) && tab.posicaoValida(p2) &&
                         tab.peca(p1) == null && tab.peca(p2) == null)
                     {
-                        mat[pos.linha, pos.coluna + 2] = true;
+                        mat[posicao.linha, posicao.coluna + 2] = true;
                     }
                 }
 
-                Posicao T2 = new Posicao(pos.linha, pos.coluna - 4);
+                Posicao T2 = new Posicao(posicao.linha, posicao.coluna - 4);
                 if (tab.posicaoValida(T2) && testeRoque(T2))
                 {
-                    Posicao p1 = new Posicao(pos.linha, pos.coluna - 1);
-                    Posicao p2 = new Posicao(pos.linha, pos.coluna - 2);
-                    Posicao p3 = new Posicao(pos.linha, pos.coluna - 3);
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
                     if (tab.posicaoValida(p1) && tab.posicaoValida(p2) && tab.posicaoValida(p3) &&
                         tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
                     {
-                        mat[pos.linha, pos.coluna - 2] = true;
+                        mat[posicao.linha, posicao.coluna - 2] = true;
                     }
                 }
             }
